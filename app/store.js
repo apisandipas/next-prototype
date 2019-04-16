0e78de673b2e4c246aac40a1e1fc32613da7ef3e
Bryan Paronto <bparonto@gmail.com>
2019-04-15 21:15:54 -0500

    redux devtools extension middlware

--- app/store.js
+++ app/store.js
@@ -1,7 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
-import reducer from "./reducers";
 import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducer from "./reducers";
 
 export const makeStore = (initialState = {}) => {
-  return createStore(reducer, initialState, applyMiddleware(thunkMiddleware));
+  return createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+  );
 };
